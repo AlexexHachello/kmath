9dd7f41a9789d83e0c7c29e4f89108cb8f07d1cc
Alexey Nikolaev <nikolaev.aa@phystech.edu>
2024-05-20 21:52:06 +0300

    The implementation for ZScore was corrected

--- kmath-stat/src/commonMain/kotlin/space/kscience/kmath/stat/ZScore.kt
+++ kmath-stat/src/commonMain/kotlin/space/kscience/kmath/stat/ZScore.kt
@@ -9,25 +9,41 @@ import space.kscience.kmath.operations.*
 import space.kscience.kmath.structures.Buffer
 
 /**
- * Variance
+ * Z-Score function
  */
-public class Variance<T>(
+public class ZScore<T>(
     private val field: Field<T>,
-) : BlockingStatistic<T, T> {
+    private val index: Int)
+    : BlockingStatistic<T, Double>{
 
-    override fun evaluateBlocking(data: Buffer<T>): T = with(field) {
-        val mean = Mean(field).evaluateBlocking(data)
-        SquaredMean(field).evaluateBlocking(data) - mean * mean
-    }
+    public override fun evaluateBlocking(data: Buffer<T>): Double = when (data.size) {
+        0 -> error("Can't compute z-score of an empty buffer")
+        1 -> {
+            if (index ==  1) 0.0 else error("Index is out of range")
+        }
+        else -> with(field){
+            if (index >= data.size){
+                error("Index is out of range")
+            }
+            else {
+                val std = StandardDeviation(field).evaluateBlocking(data)
 
-    public companion object {
-        public fun evaluate(buffer: Buffer<Double>): Double = Float64Field.variance.evaluateBlocking(buffer)
-        public fun evaluate(buffer: Buffer<Int>): Int = Int32Ring.variance.evaluateBlocking(buffer)
-        public fun evaluate(buffer: Buffer<Long>): Long = Int64Ring.variance.evaluateBlocking(buffer)
+                if (std != 0.0) {
+                    val mean = Mean(field).evaluateBlocking(data).toString().toDoubleOrNull()
+                    val elem = data[index].toString().toDoubleOrNull()
+
+                    if (mean != null && elem != null) {
+                        (elem - mean) / std
+                    } else error("ZScore should be parsed to Double")
+                } else {
+                    error("Zero division!")
+                }
+            }
+        }
     }
 }
 
 //TODO replace with optimized version which respects overflow
-public val Float64Field.variance: Variance<Double> get() = Variance(Float64Field)
-public val Int32Ring.variance: Variance<Int> get() = Variance(Int32Field)
-public val Int64Ring.variance: Variance<Long> get() = Variance(Int64Field)
+public fun Float64Field.zScore(index: Int): ZScore<Double> = ZScore(Float64Field, index)
+public fun Int32Ring.zScore(index: Int): ZScore<Int> = ZScore(Int32Field, index)
+public fun Int64Ring.zScore(index: Int): ZScore<Long> = ZScore(Int64Field, index)
